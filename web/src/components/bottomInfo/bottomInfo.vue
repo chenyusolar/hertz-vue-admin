6ce3eee22f880fada41e6bd30b33b86346632085
z4network <z4network.project@gmail.com>
2025-11-19 08:10:36 -0800

    fixed bottom

--- web/src/components/bottomInfo/bottomInfo.vue
+++ web/src/components/bottomInfo/bottomInfo.vue
@@ -1,6 +1,5 @@
 <!--
-此文件受版权保护，未经授权禁止修改！如果您尚未获得授权，请通过微信(shouzi_1994)联系我们以购买授权。在未授权状态下，只需保留此代码，不会影响任何正常使用。
-     未经授权的商用使用可能会被我们的资产搜索引擎爬取，并可能导致后续索赔。索赔金额将不低于高级授权费的十倍。请您遵守版权法律法规，尊重知识产权。
+
  -->
 <template>
   <div
@@ -11,8 +10,8 @@
       <span>
         <a
           class="font-bold text-active"
-          href="https://github.com/flipped-aurora/gin-vue-admin"
-          >Gin-Vue-Admin</a
+          href="https://github.com/chenyusolar/hertz-vue-admin"
+          >Hertz-Vue-Admin</a
         >
       </span>
     </div>
@@ -22,8 +21,8 @@
       <span>
         <a
           class="font-bold text-active"
-          href="https://github.com/flipped-aurora"
-          >flipped-aurora团队</a
+          href="https://github.com/chenyusolar"
+          >chenyusolar团队</a
         >
       </span>
     </div>
@@ -36,7 +35,7 @@
   })
 
   console.log(
-    `%c powered by %c flipped-aurorae %c`,
+    `%c powered by %c Chenyusolar %c`,
     'background:#0081ff; padding: 1px; border-radius: 3px 0 0 3px; color: #fff',
     'background:#354855; padding: 1px 5px; border-radius: 0 3px 3px 0; color: #fff; font-weight: bold;',
     'background:transparent'
